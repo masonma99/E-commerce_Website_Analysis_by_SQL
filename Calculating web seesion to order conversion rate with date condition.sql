5b4e6c3b4351787e200255a980027f59108ca15b
MasonMA <108998129+masonma99@users.noreply.github.com>
2023-01-28 23:06:59 -0500

    Update Calculating web seesion to order conversion rate with date condition.sql

--- Calculating web seesion to order conversion rate with date condition.sql
+++ Calculating web seesion to order conversion rate with date condition.sql
@@ -1,10 +1,10 @@
 SELECT 
 	COUNT(website_sessions.website_session_id) AS sessions,
-    COUNT(orders.order_id) AS orders,
-    COUNT(order_id) / COUNT(website_sessions.website_session_id) AS session_to_order_conv_rt
+	COUNT(orders.order_id) AS orders,
+	COUNT(order_id) / COUNT(website_sessions.website_session_id) AS session_to_order_conv_rt
 FROM website_sessions
 	LEFT JOIN orders
-    ON orders.website_session_id = website_sessions.website_session_id
+    	ON orders.website_session_id = website_sessions.website_session_id
 WHERE website_sessions.created_at < '2012-04-14' AND utm_source = 'gsearch' AND utm_campaign = 'nonbrand'
 GROUP BY utm_source, utm_campaign 
 	
